ff363745b3a1b56d3fd038fb585c454689f1de88
Levertion <26185209+Levertion@users.noreply.github.com>
2017-10-07 16:54:01 +0100

    Finish mcmeta support Changes global setting in order to work properly

--- src/extension.ts
+++ src/extension.ts
@@ -7,11 +7,11 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     let config = vscode.workspace.getConfiguration()
-    if (config.get("files.associations")["*.mcmeta"] == undefined && !context.globalState.get("mcmeta- updated")) {
-        let object = config.inspect("files.associations").globalValue;
+    if (config.get("files.associations")["*.mcmeta"] == undefined && !context.globalState.get("mcmeta-updated")) {
+        let object = typeof config.inspect("files.associations").globalValue == "object" ? config.inspect("files.associations").globalValue : {};
         object["*.mcmeta"] = "json";
         config.update("files.associations", object, true);
-        vscode.window.showInformationMessage("In order for the vscode-mcjson extension to support mcmeta files, it has had to update the `files.association` setting in user settings");
+        vscode.window.showInformationMessage("In order for the vscode-mcjson extension to support mcmeta files, it has had to update the `files.association` setting in user settings to make mcmeta files use JSON");
     }
     context.globalState.update("mcmeta-updated", true);
 }
